576da5f8a6834d0ab9f73356e8dcd276d6a4dd86
Aurélien Rainone <aurelien.rainone@gmail.com>
2015-09-05 03:18:55 +0200

    add vim go syntax/ft, create dstdir if not exists
    some dot 'files' are in fact dot folders, like $HOME/.vim
    in this case if one wants to manage some vim syntax or colors
    files with dotfiles, now makesymlinks.sh will create the
    destination directories if they don't exist, before creating the
    symlinks

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,8 +8,10 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles-bak             # old dotfiles backup directory
+
 # list of files/folders to symlink in homedir
-files="bash_aliases bashrc gitconfig profile machine_specific pylintrc screenrc vimrc tmux.conf tmux-git vim/colors/sourcerer.vim" 
+# TODO: when the list will start to grow, too much, create the list on the fly by taking everthing under dotfiles dir, excluding repo files
+files="bash_aliases bashrc gitconfig profile machine_specific pylintrc screenrc vimrc tmux.conf tmux-git vim/colors/sourcerer.vim vim/ftdetect/go.vim vim/syntax/go.vim" 
 ##########
 
 # create $olddir in homedir
@@ -22,47 +24,22 @@ echo -n "Changing to the $dir directory ..."
 cd $dir
 echo "done"
 
-# move any existing dotfiles in homedir to $olddir directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+# move any existing dotfiles in homedir to $olddir directory,
+# check that destination directories exist, or create them
+# then create symlinks from the homedir to any files in
+# the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file $olddir/
-    echo "Creating symlink to $file in home directory."
+	
+	# check that destination directory exists
+	dstdir=$(dirname ~/.$file)
+	if [ ! -d $dstdir ]; then
+		echo $dstdir doesn\'t exist, create it
+		mkdir -p "${dstdir}"
+	fi
 
-	# TODO: add a test to see if destination directory exists, if thats not the case, mkdir -p
-
-    ln -s $dir/$file ~/.$file
+	echo "Creating symlink to $file in home directory."
+	ln -s $dir/$file ~/.$file
 done
 
-install_zsh () {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-        git clone http://github.com/robbyrussell/oh-my-zsh.git
-    fi
-    # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        chsh -s $(which zsh)
-    fi
-else
-    # If zsh isn't installed, get the platform of the current machine
-    platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
-    if [[ $platform == 'Linux' ]]; then
-        if [[ -f /etc/redhat-release ]]; then
-            sudo yum install zsh
-            install_zsh
-        fi
-        if [[ -f /etc/debian_version ]]; then
-            sudo apt-get install zsh
-            install_zsh
-        fi
-    # If the platform is OS X, tell the user to install zsh :)
-    elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
-    fi
-fi
-}
-
-#install_zsh
